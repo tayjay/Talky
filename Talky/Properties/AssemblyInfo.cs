f3e5cec2931c205919c4ebac75fdb4a1a0f9363c
tayjay <tay.j.masson@gmail.com>
2026-05-30 13:50:59 -0700

    Bump Talky to 0.9.0.

--- Talky/Properties/AssemblyInfo.cs
+++ Talky/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TayTay")]
 [assembly: AssemblyProduct("Talky")]
-[assembly: AssemblyCopyright("Copyright © TayTay 2025")]
+[assembly: AssemblyCopyright("Copyright © TayTay 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.1.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
